f2e758e4372e28f22e98d66d0f52b41f73b0df55
thefeymesaleng <thefeymesaleng@gmail.com>
2024-05-30 21:45:43 +0800

    test

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="size-full flex justify-center items-center"
-    style="display: flex; width: 100%"
+    class="size-full flex items-center justify-center"
+    style="display: flex; width: 100%; height: 100%"
   >
     <NuxtWelcome />
   </div>
