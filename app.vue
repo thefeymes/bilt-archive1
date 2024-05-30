179faaafd639f14275e06300cf76499c288f2945
thefeymesaleng <thefeymesaleng@gmail.com>
2024-05-30 21:24:08 +0800

    another lint test

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="size-full flex justify-center items-center">
+  <div
+    class="size-full flex justify-center items-center"
+    style="display: flex; width: 100%"
+  >
     <NuxtWelcome />
   </div>
 </template>
